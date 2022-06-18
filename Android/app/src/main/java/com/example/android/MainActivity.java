f202e1c8f5672d7eb0bcff499d7343678d80b1a6
Federico <federico.guidazzi@studio.unibo.it>
2022-06-18 19:02:10 +0200

    finished the appearence of the login page

--- Android/app/src/main/java/com/example/android/MainActivity.java
+++ Android/app/src/main/java/com/example/android/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //check sulle credenziali
-                if(username.getText().equals("ciao") && password.getText().equals("ciao") ){
+                if(username.getText().toString().equals("ciao") && password.getText().toString().equals("ciao") ){
                     Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
