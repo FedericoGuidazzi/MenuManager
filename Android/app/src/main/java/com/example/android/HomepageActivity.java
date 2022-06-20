bdf69324ecb1df0182abff94697a7e025029f06f
Federico <federico.guidazzi@studio.unibo.it>
2022-06-20 15:26:59 +0200

    added input for insert daily menu

--- Android/app/src/main/java/com/example/android/HomepageActivity.java
+++ Android/app/src/main/java/com/example/android/HomepageActivity.java
@@ -1,7 +1,11 @@
 package com.example.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,17 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendar);
+
+        //listener per il cambio data
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+
+                Toast.makeText(getApplicationContext(), ""+dayOfMonth+" "+(month+1)+" "+year, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
