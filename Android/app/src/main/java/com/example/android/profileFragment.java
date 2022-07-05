2a283e4d9841a776cfda113d09f912db9a601553
FedericoGuidazzi <federico.guidazzi@studio.unibo.it>
2022-07-05 19:38:02 +0200

    modified profile view username

--- Android/app/src/main/java/com/example/android/profileFragment.java
+++ Android/app/src/main/java/com/example/android/profileFragment.java
@@ -3,12 +3,18 @@ package com.example.android;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.Database.userRepository;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +24,8 @@ import android.widget.Toast;
 public class profileFragment extends Fragment {
 
     TextView usernameTextView;
+    userRepository userRepository;
+    LiveData<List<User>> userList;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -52,6 +60,8 @@ public class profileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        userRepository = new userRepository(getActivity().getApplication());
+        userList = userRepository.getUser();
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -64,7 +74,17 @@ public class profileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         usernameTextView = view.findViewById(R.id.usernameText);
-        usernameTextView.setText(String.valueOf(((GlobalClass) this.getActivity().getApplication()).getUserId()));
+        userList.observe(getActivity(), new Observer<List<User>>() {
+            @Override
+            public void onChanged(List<User> users) {
+                for(User user: users){
+                    if(user.id == ((GlobalClass) getActivity().getApplication()).getUserId() ){
+                        usernameTextView.setText(user.username);
+                        break;
+                    }
+                }
+            }
+        });
         return view;
     }
 }
